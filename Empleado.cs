99bd744358e815bd44ca4f5e063fbfcf25c7d763
Anuar Ricardo <anuarricardo53@gmail.com>
2024-05-05 19:13:51 -0600

    Mi cambio

---
+++ Empleado.cs
@@ -0,0 +1,52 @@
+﻿internal class Empleado
+{
+    private string nombre;
+    private int numeroEmpleado;
+    private decimal salario;
+    private bool activo;
+
+    public Empleado(string nombre, int numeroEmpleado, decimal salario, bool activo)
+    {
+        this.nombre = nombre;
+        this.numeroEmpleado = numeroEmpleado;
+        this.salario = salario;
+        this.activo = activo;
+    }
+
+    public void MostrarDetalles()
+    {
+        string estado = (activo) ? "Activo" : "Inactivo";
+        Console.WriteLine($"Nombre: {nombre}, Número de empleado: {numeroEmpleado}, Salario: {salario:C}, Estado: {estado}");
+    }
+
+    public int ObtenerNumeroEmpleado()
+    {
+        return this.numeroEmpleado;
+    }
+
+    public void CambiarEstado(int nuevoEstado)
+    {
+        if (nuevoEstado == 1)
+        {
+            activo = true;
+            Console.WriteLine("Empleado activado");
+        }
+        else if (nuevoEstado == 0)
+        {
+            activo = false;
+            Console.WriteLine("Empleado desactivado");
+        }
+        else
+        {
+            Console.WriteLine("ERROR: Estado inválido");
+        }
+    }
+
+    public void AumentoSalario(double porcentaje)
+    {
+        decimal aumento = salario * Convert.ToDecimal(0.15);
+        salario += aumento;
+        Console.WriteLine($"Salario aumentado en un {porcentaje}%");
+        Console.WriteLine($"Nuevo salario para {nombre}: {salario:C}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("App de gestión de empleados");
+        List<Empleado> listaEmpleados = new List<Empleado>();
+        listaEmpleados.Add(new Empleado("Juan", 101, 30000, true));
+        listaEmpleados.Add(new Empleado("Maria", 102, 35000, true));
+        listaEmpleados.Add(new Empleado("Pedro", 103, 32000, false));
+
+        foreach (var empleado in listaEmpleados)
+        {
+            empleado.MostrarDetalles();
+        }
+
+        int numeroEmpleadoCambiarEstado = 103;
+        Empleado empleadoCambiarEstado = BuscarEmpleado(listaEmpleados, numeroEmpleadoCambiarEstado);
+
+        if (empleadoCambiarEstado != null)
+        {
+            empleadoCambiarEstado.CambiarEstado(1);
+            empleadoCambiarEstado.MostrarDetalles();
+            empleadoCambiarEstado.AumentoSalario(15); 
+        }
+        else
+        {
+            Console.WriteLine($"Empleado con número {numeroEmpleadoCambiarEstado} no encontrado");
+        }
+    }
+
+    static Empleado BuscarEmpleado(List<Empleado> empleados, int numeroEmpleado)
+    {
+        foreach (var empleado in empleados)
+        {
+            if (empleado.ObtenerNumeroEmpleado() == numeroEmpleado)
+            {
+                return empleado;
+            }
+        }
+        return null;
+    }
+}
